2c9fe5c653b184e16c3e4ceb9fac1443801839be
Warre Dujardin <warre.dujardin@gmail.com>
2025-08-15 17:04:50 +0200

    hyprland

---
+++ dot_config/hypr/scripts/executable_listener.fish
@@ -0,0 +1,27 @@
+#!/usr/bin/env fish
+
+function handleLine
+    if string match -rq '^(?<event>.*)>>(?<args>.*)$' -- "$argv"
+        if string match -raq '(?<vars>[^,]*),?' -- "$args"
+            handleEvent $event $vars
+        end
+    end
+end
+
+function handleEvent
+    set -l event $argv[1]
+    set -l vars $argv[2..]
+
+    if test $LOG_EVENTS
+        echo $event $vars
+    end
+
+    # make Firefox extension windows float
+    if test $event = windowtitlev2 && string match -rq 'Extension.*Mozilla Firefox' -- "$vars[2]"
+        hyprctl dispatch setfloating address:0x$vars[1]
+    end
+end
+
+socat - "UNIX-CONNECT:$XDG_RUNTIME_DIR/hypr/$HYPRLAND_INSTANCE_SIGNATURE/.socket2.sock" | while read -l line
+    handleLine "$line"
+end
---
+++ dot_config/hypr/scripts/executable_screenshot.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Set the screenshot directory and file name
+DIR=~/Pictures/Screenshots
+FILE="Screenshot-$(date +%m-%d-%YT%H:%M:%S)-$(head /dev/urandom | tr -dc a-f | head -c 4).png"
+SAVE_TO="$DIR/$FILE"
+
+# Create the Screenshots directory if it doesn't exist
+mkdir -p $DIR
+
+# Take a screenshot with grim (and slurp for selection)
+grim -g "$(slurp -o -c 04d9ff -w 0)" $SAVE_TO
+# Send a notification if grim was successful
+if [ $? -eq 0 ]; then
+    notify-send -i "$SAVE_TO" "Screenshot captured" "Copied to clipboard\nSaved to $SAVE_TO" 
+    wl-copy < $DIR/$FILE
+fi
