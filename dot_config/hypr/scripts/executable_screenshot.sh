fc26b3976252456eb8a35914f8f36b8ecabe6ad3
Warre Dujardin <warre.dujardin@gmail.com>
2025-08-18 20:40:20 +0200

    make screenshots even nicer!

--- dot_config/hypr/scripts/executable_screenshot.sh
+++ dot_config/hypr/scripts/executable_screenshot.sh
@@ -1,17 +1,16 @@
-#!/bin/bash
+#!/usr/bin/env fish
 
-# Set the screenshot directory and file name
-DIR=~/Pictures/Screenshots
-FILE="Screenshot-$(date +%m-%d-%YT%H:%M:%S)-$(head /dev/urandom | tr -dc a-f | head -c 4).png"
-SAVE_TO="$DIR/$FILE"
+set -l DIR ~/Pictures/Screenshots
+set -l FILE Screenshot-(date +%m-%d-%YT%H:%M:%S)-(head /dev/urandom | tr -dc a-f | head -c 4).png
+set -l SAVE_TO "$DIR/$FILE"
 
-# Create the Screenshots directory if it doesn't exist
 mkdir -p $DIR
 
-# Take a screenshot with grim (and slurp for selection)
-grim -g "$(slurp -o -b '#ffffff20' -B '#ffffff20' -s '#ffffff40' -w 0 -d)" $SAVE_TO
-# Send a notification if grim was successful
-if [ $? -eq 0 ]; then
-  notify-send -i "$SAVE_TO" "Screenshot captured" "Copied to clipboard\nSaved to $SAVE_TO"
-  wl-copy <$DIR/$FILE
-fi
+set -l region (slurp -o -b '#ffffff20' -B '#ffffff20' -s '#ffffff40' -w 0 -d)
+echo $region
+grim -g "$region" $SAVE_TO
+
+if test $status -eq 0
+    notify-send -i "$SAVE_TO" "Screenshot captured" "Copied to clipboard\nSaved to $SAVE_TO"
+    wl-copy < $SAVE_TO
+end
