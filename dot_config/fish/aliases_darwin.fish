cacc1d447ae9e3dda68060ff68a5cedbb5d89900
Warre Dujardin <warre.dujardin@techwolf.ai>
2025-08-21 15:06:33 +0200

    yoink files for mac

--- dot_config/fish/aliases_darwin.fish
+++ dot_config/fish/aliases_darwin.fish
@@ -1,1 +1,7 @@
 alias mintlify "pnpm dlx mintlify"
+
+function yoink -a file
+    set -l absolute_path $(realpath $file)
+    osascript -e "set the clipboard to (POSIX file \"$absolute_path\")"
+    echo "Copied \"$file\" to clipboard."
+end
